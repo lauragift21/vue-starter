7e360d267ecbfd0ca2047c7ad8351098c840e4cf
egwuenu gift <laurandidi21@gmail.com>
2017-10-13 22:39:43 +0100

    added nuxt directory

--- vue-course/my-project/src/App.vue
+++ vue-course/my-project/src/App.vue
@@ -1,60 +1,119 @@
 <template>
   <div id="app">
-    <img src="./assets/logo.png">
-    <h1>{{ msg }}</h1>
-    <h2>Essential Links</h2>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank">Forum</a></li>
-      <li><a href="https://gitter.im/vuejs/vue" target="_blank">Gitter Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank">Twitter</a></li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li><a href="http://router.vuejs.org/" target="_blank">vue-router</a></li>
-      <li><a href="http://vuex.vuejs.org/" target="_blank">vuex</a></li>
-      <li><a href="http://vue-loader.vuejs.org/" target="_blank">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank">awesome-vue</a></li>
-    </ul>
+    <div class="unit"  v-for="unit in manifest">
+      <child :item="unit.item" :url="unit.url"></child>
+    </div>
   </div>
 </template>
 
 <script>
+import Child from './components/Child.vue';
 export default {
   name: 'app',
-  data () {
+  components: {
+    Child
+  },
+  data() {
     return {
-      msg: 'Welcome to Your Vue.js App'
-    }
+      manifest: [
+        {
+          item: "backpack",
+          url: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/backpack.jpg"
+        },
+        {
+          item: "tshirt",
+          url: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/tshirt.jpg"
+        },
+         {
+          item: "sweatshirt",
+          url: "https://s3-us-west-2.amazonaws.com/s.cdpn.io/28963/sweatshirt.jpg"
+        },
+      ]
+    };
   }
 }
 </script>
 
 <style lang="scss">
+body, html {
+  font-family: 'PT Serif', serif;
+}
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
+  display: flex;
+  justify-content: center;
+}
+.special {
+  font-family: 'Montserrat', sans-serif;
+  text-transform: uppercase;
+  letter-spacing: 0.08em;
+  font-size: 12px;
+}
+h1, h2, h3, li {
+  @extend .special;
+}
+h2 {
+  font-size: 20px;
+  margin: 0 0 5px;
   text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
 }
-
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
 li {
+  list-style: none;
   display: inline-block;
-  margin: 0 10px;
 }
-
-a {
-  color: #42b983;
+input {
+  background: #eee;
+}
+button {
+  border: none;
+  border-radius: 1000px;
+  padding: 8px 13px;
+  outline: none;
+  cursor: pointer;
+  &.inc {
+    background: white;
+    color: black;
+  }
+  &.submit {
+    background: black;
+    color: white;
+    @extend .special;
+    margin-left: 20px;
+    padding: 12px 14px;
+    width: 240px;
+    transition: 0.25s all ease-in;
+    &:hover {
+      opacity: 0.8;
+      transition: 0.25s all ease-out;
+    }
+  }
+}
+.quant-text {
+  display: inline-block;
+  text-align: center;
+  width: 140px;
+  padding: 8px;
+  padding: 8px;
+  @extend .special;
+}
+.quantity {
+  border: 2px solid black;
+  width: 235px;
+  padding-left: 3px;
+  border-radius: 1000px;
+  background: #fff;
+  margin-left: 20px;
+}
+.item {
+  display: flex;
+  flex-direction: column;
+  width: 280px;
+  height: 440px;
+  justify-content: space-around;
+  background: #eee;
+  padding: 20px 10px;
+  margin: 30px;
+  img {
+    align-self: center;
+  }
 }
 </style>
